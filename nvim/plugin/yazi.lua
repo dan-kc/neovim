7d5be375315333414b6884a2a7838b9fde367df7
Daniel Cox <63171098+dan-kc@users.noreply.github.com>
2025-05-09 16:14:02 +0100

    disable yazi for now

--- nvim/plugin/yazi.lua
+++ nvim/plugin/yazi.lua
@@ -1,11 +1,11 @@
-if vim.g.did_load_yazi_plugin then
-  return
-end
-vim.g.did_load_yazi_plugin = true
+-- if vim.g.did_load_yazi_plugin then
+--   return
+-- end
+-- vim.g.did_load_yazi_plugin = true
 
-local yazi = require('yazi')
-vim.keymap.set('n', '<leader>e', '<cmd>Yazi<cr>', { desc = 'Open Yazi' })
-vim.keymap.set('n', '<leader>E', '<cmd>Yazi cwd<cr>', { desc = 'Open Yazi CWD' })
-yazi.setup {
-  open_for_directories = false,
-}
+-- local yazi = require('yazi')
+-- vim.keymap.set('n', '<leader>e', '<cmd>Yazi<cr>', { desc = 'Open Yazi' })
+-- vim.keymap.set('n', '<leader>E', '<cmd>Yazi cwd<cr>', { desc = 'Open Yazi CWD' })
+-- yazi.setup {
+--   open_for_directories = false,
+-- }
