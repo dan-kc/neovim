4aabca53da14f890f01231b5251fae2fa1f6893f
Daniel Cox <63171098+dan-kc@users.noreply.github.com>
2025-05-15 21:43:50 +0100

    clean up

--- nvim/plugin/conform.lua
+++ nvim/plugin/conform.lua
@@ -5,7 +5,6 @@ vim.g.did_load_conform_plugin = true
 
 require('conform').setup {
   formatters_by_ft = {
-    -- Configured
     lua = { 'stylua' },
     nix = { 'nixfmt' },
     rust = { 'rustfmt' },
@@ -28,33 +27,9 @@ require('conform').setup {
     graphql = { 'prettier' },
     handlebars = { 'prettier' },
     liquid = { 'prettier' },
-
-    -- To be configured
     toml = { 'taplo' },
-
-    -- proto = { 'buf' },
   },
-  -- ft_parsers = {
-  --   html = 'html',
-  -- },
   formatters = {
-    shopify_liquid_formatter = {
-      command = 'shopify',
-      args = { 'theme', 'format', '--path', '$FILENAME', '--output', '$TEMP', '--json' },
-      -- This is crucial for handling the output of the shopify command
-      -- The shopify theme format --json outputs a JSON object with a "content" key
-      -- containing the formatted code. We need to extract that.
-      stdout = function(args)
-        -- Use jq to parse the JSON output and extract the "content" field
-        -- If jq is not available, you might need a more complex shell command
-        -- to parse the JSON.
-        local handle = io.popen("echo '" .. args.stdout:gsub("'", "'\\''") .. "' | jq -r '.content'")
-        local output = handle:read('*a')
-        handle:close()
-        return output
-      end,
-      -- Optional: Configure stderr or other options if needed
-    },
     tfmt = {
       command = 'tofu',
       args = { 'fmt', '-' },
