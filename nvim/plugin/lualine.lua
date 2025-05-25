4c0eca06a1fde2af043798540842d52270907f26
Daniel Cox <63171098+dan-kc@users.noreply.github.com>
2025-05-25 16:55:15 +0100

    show recording in lualine

--- nvim/plugin/lualine.lua
+++ nvim/plugin/lualine.lua
@@ -4,32 +4,41 @@ end
 vim.g.did_load_lualine_plugin = true
 local icons = require('user.icons')
 
+local function show_macro_recording()
+  local recording_register = vim.fn.reg_recording()
+  if recording_register == '' then
+    return ''
+  else
+    return 'Recording @' .. recording_register
+  end
+end
+
 require('lualine').setup {
   options = {
-    theme = "auto",
-    component_separators = "",
-    section_separators = "",
+    theme = 'auto',
+    component_separators = '',
+    section_separators = '',
   },
   sections = {
-    lualine_a = { { "mode" } },
-    lualine_b = { { "location", "progress" } },
+    lualine_a = { 'mode' },
+    lualine_b = { 'location', { show_macro_recording } },
     lualine_c = {
       {
-        "diagnostics",
-        sections = { "error", "warn", "info", "hint" },
-        sources = { "nvim_diagnostic" },
+        'diagnostics',
+        sections = { 'error', 'warn', 'info', 'hint' },
+        sources = { 'nvim_diagnostic' },
         symbols = {
-          error = string.format("%s%s", icons.more.error, " "),
-          warn = string.format("%s%s", icons.more.warningCircle, " "),
-          info = string.format("%s%s", icons.more.info, " "),
+          error = string.format('%s%s', icons.more.error, ' '),
+          warn = string.format('%s%s', icons.more.warningCircle, ' '),
+          info = string.format('%s%s', icons.more.info, ' '),
         },
       },
     },
     lualine_x = {
-      "lsp_progress",
+      'lsp_progress',
     },
-    lualine_y = { { "encoding" } },
-    lualine_z = { { "branch", icon = icons.git.symbol } },
+    lualine_y = { { 'encoding' } },
+    lualine_z = { { 'branch', icon = icons.git.symbol } },
   },
   extensions = { 'fugitive', 'fzf', 'toggleterm', 'quickfix' },
 }
