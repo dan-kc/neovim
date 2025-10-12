37b06cd8d16ac8fff86cc3361fc8db11e99694ca
Daniel Cox <63171098+dan-kc@users.noreply.github.com>
2025-10-12 16:50:28 +0100

    half working

--- nvim/plugin/luasnip.lua
+++ nvim/plugin/luasnip.lua
@@ -7,16 +7,39 @@ local ls = require('luasnip')
 local i = ls.insert_node
 local t = ls.text_node
 local s = ls.snippet
+local f = ls.function_node
+
 vim.keymap.set({ 'i' }, '<C-L>', function()
   ls.expand()
 end, { silent = true })
-ls.add_snippets('all', {
-  s('ternary', {
-    -- equivalent to "${1:cond} ? ${2:then} : ${3:else}"
-    i(1, 'cond'),
-    t(' ? '),
-    i(2, 'then'),
-    t(' : '),
-    i(3, 'else'),
-  }),
-})
+
+local function get_indent()
+  return vim.fn.indent(vim.fn.line('.'))
+end
+
+local closing_brackets = {
+  ['('] = ')',
+  ['['] = ']',
+  ['<'] = '>',
+  ['{'] = '}',
+}
+local bracket_snippets = {}
+for open, close in pairs(closing_brackets) do
+  table.insert(
+    bracket_snippets,
+    s(open, {
+      t({ open .. ' ', '' }),
+      f(function()
+        return string.rep(' ', get_indent() + vim.o.shiftwidth)
+      end),
+      i(1),
+      t({ '', '' }),
+      f(function()
+        return string.rep(' ', get_indent())
+      end),
+      t(close),
+    })
+  )
+end
+
+ls.add_snippets('all', bracket_snippets)
