bbf10f29daff5ca096f64619f975e19ccc4b90c7
Daniel Cox <63171098+dan-kc@users.noreply.github.com>
2026-01-16 12:34:19 +0000

    add next chunk commands

--- nvim/lua/configs/gitsigns.lua
+++ nvim/lua/configs/gitsigns.lua
@@ -3,6 +3,36 @@ return {
   after = function()
     require('gitsigns').setup {
       current_line_blame = false,
+      on_attach = function(bufnr)
+        local gs = package.loaded.gitsigns
+
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+
+        -- Navigation
+        map('n', ']g', function()
+          if vim.wo.diff then
+            return ']g'
+          end
+          vim.schedule(function()
+            gs.next_hunk()
+          end)
+          return '<Ignore>'
+        end, { expr = true })
+
+        map('n', '[g', function()
+          if vim.wo.diff then
+            return '[g'
+          end
+          vim.schedule(function()
+            gs.prev_hunk()
+          end)
+          return '<Ignore>'
+        end, { expr = true })
+      end,
     }
   end,
 }
