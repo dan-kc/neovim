c635067f1ebd2977bea51cfe36bc7b8bcb187ca2
Daniel Cox <63171098+dan-kc@users.noreply.github.com>
2026-02-06 10:58:09 +0000

    update

--- nvim/plugin/treesitter.lua
+++ nvim/plugin/treesitter.lua
@@ -3,91 +3,71 @@ if vim.g.did_load_treesitter_plugin then
 end
 vim.g.did_load_treesitter_plugin = true
 
-local configs = require('nvim-treesitter.configs')
 vim.g.skip_ts_context_comment_string_module = true
 
----@diagnostic disable-next-line: missing-fields
-configs.setup {
-  -- ensure_installed = 'all',
-  -- auto_install = false, -- Do not automatically install missing parsers when entering buffer
-  highlight = {
-    enable = true,
-    disable = function(_, buf)
-      local max_filesize = 100 * 1024 -- 100 KiB
-      local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-      if ok and stats and stats.size > max_filesize then
-        return true
-      end
-    end,
-  },
-  textobjects = {
-    select = {
-      enable = true,
-      -- Automatically jump forward to textobject, similar to targets.vim
-      lookahead = true,
-      keymaps = {
-        ['af'] = '@function.outer',
-        ['if'] = '@function.inner',
-        ['ac'] = '@class.outer',
-        ['ic'] = '@class.inner',
-        ['aC'] = '@call.outer',
-        ['iC'] = '@call.inner',
-        ['a#'] = '@comment.outer',
-        ['i#'] = '@comment.outer',
-        ['ai'] = '@conditional.outer',
-        ['ii'] = '@conditional.outer',
-        ['al'] = '@loop.outer',
-        ['il'] = '@loop.inner',
-        ['aP'] = '@parameter.outer',
-        ['iP'] = '@parameter.inner',
-      },
-      selection_modes = {
-        ['@parameter.outer'] = 'v', -- charwise
-        ['@function.outer'] = 'V', -- linewise
-        ['@class.outer'] = '<c-v>', -- blockwise
-      },
-    },
-    swap = {
-      enable = true,
-      swap_next = {
-        ['<leader>a'] = '@parameter.inner',
-      },
-      swap_previous = {
-        ['<leader>A'] = '@parameter.inner',
-      },
-    },
-    move = {
-      enable = true,
-      set_jumps = true, -- whether to set jumps in the jumplist
-      goto_next_start = {
-        [']m'] = '@function.outer',
-        [']P'] = '@parameter.outer',
-      },
-      goto_next_end = {
-        [']m'] = '@function.outer',
-        [']P'] = '@parameter.outer',
-      },
-      goto_previous_start = {
-        ['[m'] = '@function.outer',
-        ['[P'] = '@parameter.outer',
-      },
-      goto_previous_end = {
-        ['[m'] = '@function.outer',
-        ['[P'] = '@parameter.outer',
-      },
-    },
-    nsp_interop = {
-      enable = true,
-      peek_definition_code = {
-        ['df'] = '@function.outer',
-        ['dF'] = '@class.outer',
-      },
+-- Disable treesitter highlighting for large files
+vim.api.nvim_create_autocmd('FileType', {
+  callback = function(args)
+    local max_filesize = 100 * 1024 -- 100 KiB
+    local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(args.buf))
+    if ok and stats and stats.size > max_filesize then
+      vim.treesitter.stop(args.buf)
+    end
+  end,
+})
+
+-- Textobjects configuration (nvim-treesitter-textobjects)
+require('nvim-treesitter-textobjects').setup {
+  select = {
+    lookahead = true,
+    selection_modes = {
+      ['@parameter.outer'] = 'v', -- charwise
+      ['@function.outer'] = 'V', -- linewise
+      ['@class.outer'] = '<c-v>', -- blockwise
     },
   },
 }
 
+-- Textobject keymaps
+local select_keymaps = {
+  ['af'] = '@function.outer',
+  ['if'] = '@function.inner',
+  ['ac'] = '@class.outer',
+  ['ic'] = '@class.inner',
+  ['aC'] = '@call.outer',
+  ['iC'] = '@call.inner',
+  ['a#'] = '@comment.outer',
+  ['i#'] = '@comment.outer',
+  ['ai'] = '@conditional.outer',
+  ['ii'] = '@conditional.outer',
+  ['al'] = '@loop.outer',
+  ['il'] = '@loop.inner',
+  ['aP'] = '@parameter.outer',
+  ['iP'] = '@parameter.inner',
+}
+
+for keymap, query in pairs(select_keymaps) do
+  vim.keymap.set({ 'x', 'o' }, keymap, function()
+    require('nvim-treesitter-textobjects.select').select_textobject(query)
+  end)
+end
+
+-- Swap keymaps
+vim.keymap.set('n', '<leader>a', function()
+  require('nvim-treesitter-textobjects.swap').swap_next('@parameter.inner')
+end)
+vim.keymap.set('n', '<leader>A', function()
+  require('nvim-treesitter-textobjects.swap').swap_previous('@parameter.inner')
+end)
+
+-- Move keymaps
+local move = require('nvim-treesitter-textobjects.move')
+vim.keymap.set({ 'n', 'x', 'o' }, ']m', function() move.goto_next_start('@function.outer') end)
+vim.keymap.set({ 'n', 'x', 'o' }, ']P', function() move.goto_next_start('@parameter.outer') end)
+vim.keymap.set({ 'n', 'x', 'o' }, '[m', function() move.goto_previous_start('@function.outer') end)
+vim.keymap.set({ 'n', 'x', 'o' }, '[P', function() move.goto_previous_start('@parameter.outer') end)
+
 require('ts_context_commentstring').setup()
 
--- Tree-sitter based folding
--- vim.opt.foldmethod = 'expr'
-vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
+-- Tree-sitter based folding (now built into Neovim)
+vim.opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
