4659f5a1aca9a29831c089af798838ce9f93837e
Daniel Cox <63171098+dan-kc@users.noreply.github.com>
2026-09-11 19:08:55 +0100

    Update deps and blink

--- nvim/lua/configs/blink.lua
+++ nvim/lua/configs/blink.lua
@@ -1,3 +1,39 @@
+local pending_menu_visibility
+local toggle_generation = 0
+
+local function toggle_completion_menu(cmp)
+  local menu = require('blink.cmp.completion.windows.menu')
+  local is_visible = cmp.is_menu_visible()
+
+  if pending_menu_visibility ~= nil then
+    is_visible = pending_menu_visibility
+  end
+
+  pending_menu_visibility = not is_visible
+  toggle_generation = toggle_generation + 1
+  local generation = toggle_generation
+
+  vim.schedule(function()
+    if generation ~= toggle_generation then
+      return
+    end
+
+    local should_open = pending_menu_visibility
+    pending_menu_visibility = nil
+
+    if not should_open then
+      menu.close()
+    elseif cmp.is_active() and #cmp.get_items() > 0 then
+      menu.open()
+      menu.update_position()
+    else
+      cmp.show()
+    end
+  end)
+
+  return true
+end
+
 return {
   'blink.cmp',
   after = function()
@@ -7,9 +43,7 @@ return {
         ['<C-y>'] = { 'select_and_accept', 'fallback' },
         ['<C-p>'] = { 'select_prev', 'fallback_to_mappings' },
         ['<C-n>'] = { 'select_next', 'fallback_to_mappings' },
-        ['<C-e>'] = { 'cancel', 'fallback' },
-        ['<PageUp>'] = { 'scroll_documentation_up', 'fallback' },
-        ['<PageDown>'] = { 'scroll_documentation_down', 'fallback' },
+        ['<C-e>'] = { toggle_completion_menu },
       },
       completion = {
         keyword = {
@@ -37,8 +71,8 @@ return {
             treesitter = { 'lsp' },
           },
         },
-        ghost_text = { enabled = true },
-        documentation = { auto_show = true },
+        ghost_text = { enabled = false },
+        documentation = { auto_show = false },
       },
       sources = {
         default = { 'lsp', 'path', 'buffer' },
